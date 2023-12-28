9e89c4e2eb0ae1c5ebd0816cef452b0afc22d634
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-12-28 16:05:55 +0100

    Added Header...

---
+++ src/Components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="bg-[#fff] rounded-md mt-4 mx-3 pt-8 pb-10 px-6 flex flex-col">
+        <div>
+            <span class="text-[#384D67] text-[17px] font-[700] leading-8">Header Settings</span>
+        </div>
+        <div class="flex flex-row items-center mt-3 space-x-3">
+            <div class="flex flex-col items-start justify-start w-full space-y-2">
+                <div class="flex flex-row justify-between w-full max-w-[70%]">
+                    <span class="flex flex-row items-center text-[#525252] text-[14px] font-[400] leading-[26px]">Main
+                        Keyword <p class="text-[#CA1818]">*</p></span>
+                    <span
+                        class="flex flex-row items-center text-[#525252] text-[14px] font-[400] leading-[26px]">61/80</span>
+                </div>
+                <div class="flex flex-row items-center w-full mt-3 space-x-3">
+                    <input type="text" class="bg-[#fff] border-[#CBCBCB] border-[1px] py-3 px-4 rounded-md w-[70%]"
+                        value="What do dermatologist prescribe for skin whitening?">
+                    <button
+                        class="flex w-[20%] flex-row items-center justify-center space-x-1 bg-[#77AEF4] rounded-md border-[#77AEF4] border-[1px] py-2 px-6">
+                        <p class="text-[#FFFFFF] text-[14px] font-[700] leading-8">Regenerate Title</p>
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-row items-center mt-5 space-x-3">
+            <div class="flex flex-col items-start justify-start w-full space-y-2">
+                <div class="flex flex-row justify-between w-full max-w-[70%]">
+                    <span class="flex flex-row items-center text-[#525252] text-[14px] font-[400] leading-[26px]">Title <p class="text-[#CA1818]">*</p></span>
+                    <span
+                        class="flex flex-row items-center text-[#525252] text-[14px] font-[400] leading-[26px]">70/80</span>
+                </div>
+                <div class="flex flex-row items-center w-full mt-3 space-x-3">
+                    <input type="text" class="bg-[#fff] border-[#CBCBCB] border-[1px] py-3 px-4 rounded-md w-[70%]"
+                        value="What do dermatologist prescribe for skin whitening? Discover Now!">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
--- src/Views/Projects.vue
+++ src/Views/Projects.vue
@@ -1,14 +1,18 @@
 <template>
     <div class="bg-[#F2F1F9] flex flex-col  min-h-screen mx-auto w-full max-w-[1800px]">
         <Navbar />
+        <main>
+            <Header />
+        </main>
     </div>
 </template>
 <script>
 
 import Navbar from '../Components/Navbar.vue';
+import Header from '../Components/Header.vue';
 export default {
     name: "Projects",
-    components: { Navbar, }
+    components: { Navbar, Header }
 
 }
 </script>
